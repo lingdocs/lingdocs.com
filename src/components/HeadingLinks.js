a711a2c0849c3c338291c44ada5cee1b660eefff
71590811+lingdocs@users.noreply.github.com <adam@Adams-Air.lan>
2022-08-23 15:49:19 +0400

    touchup

--- src/components/HeadingLinks.js
+++ src/components/HeadingLinks.js
@@ -1,22 +1,20 @@
 import React from "react";
 
+const btnStyle = {
+    border: "2px solid white",
+    padding: "0.75rem",
+    margin: "0 1rem",
+};
+
 function HeadingLinks() {
     return <div className="d-flex flex-row justify-content-center" style={{ marginTop: "3rem"}}>
         <a href="https://dictionary.lingdocs.com" className="plain-link">
-        <div style={{
-            border: "2px solid white",
-            padding: "0.75rem",
-            margin: "0 1rem",
-        }}>
+        <div className="text-center" style={btnStyle}>
             Pashto Dictionary
         </div>
         </a>
         <a href="https://grammar.lingdocs.com" className="plain-link">
-        <div style={{
-            border: "2px solid white",
-            padding: "0.75rem",
-            margin: "0 1rem",
-        }}>
+        <div className="text-center" style={btnStyle}>
             Pashto Grammar
         </div>
         </a>
