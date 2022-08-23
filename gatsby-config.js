568cb84922c6be8289532928adb38cc52d2d6617
71590811+lingdocs@users.noreply.github.com <adam@Adams-Air.lan>
2022-08-23 10:32:43 +0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,5 +101,22 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-FNB8658Z2J",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
   ],
 }
