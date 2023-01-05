4c980a037241d152886e1f06839c156d399a8109
adueck <adueck@users.noreply.github.com>
2023-01-05 14:52:47 +0500

    add playstore badges

--- src/components/HeadingLinks.js
+++ src/components/HeadingLinks.js
@@ -7,18 +7,25 @@ const btnStyle = {
 };
 
 function HeadingLinks() {
-    return <div className="d-flex flex-row justify-content-center" style={{ marginTop: "3rem"}}>
-        <a href="https://dictionary.lingdocs.com" className="plain-link">
-        <div className="text-center" style={btnStyle}>
-            Pashto Dictionary
+    return <>
+        <div className="d-flex flex-row justify-content-center" style={{ marginTop: "3rem"}}>
+            <a href="https://dictionary.lingdocs.com" className="plain-link">
+                <div className="text-center" style={btnStyle}>
+                    Pashto Dictionary
+                </div>
+            </a>
+            <a href="https://grammar.lingdocs.com" className="plain-link">
+                <div className="text-center" style={btnStyle}>
+                    Pashto Grammar
+                </div>
+            </a>
         </div>
-        </a>
-        <a href="https://grammar.lingdocs.com" className="plain-link">
-        <div className="text-center" style={btnStyle}>
-            Pashto Grammar
+        <div className="d-flex flex-row justify-content-center">
+            <a href='https://play.google.com/store/apps/developer?id=LingDocs&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                <img style={{ maxWidth: "15rem", paddingTop: "2rem" }} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+            </a>
         </div>
-        </a>
-    </div>;
+    </>;
 }
 
 export default HeadingLinks;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,15 +6,17 @@ import aboutBackground from "../assets/images/about-bg.jpg";
 const projects = [
     {
         title: "Pashto Grammar",
-        description: "A Pashto grammar reference. This is extremely new and only in the very beginning stages.",
+        description: "A Pashto grammar reference with interactive examples and quizzes.",
         link: "https://grammar.lingdocs.com",
         repo: "https://github.com/lingdocs/pashto-grammar",
+        playStore: "https://play.google.com/store/apps/details?id=com.lingdocs.pashto.grammar",
     },
     {
         title: "Pashto Dictionary",
         description: "A Pashto dicitionary with smart-search and inflection/conjugation",
         link: "https://dictionary.lingdocs.com",
-        repo: "https://github.com/lingdocs/pashto-dictionary"
+        repo: "https://github.com/lingdocs/pashto-dictionary",
+        playStore: "https://play.google.com/store/apps/details?id=com.lingdocs.pashto.dictionary",
     },
     {
       title: "Pashto Inflector",
@@ -41,7 +43,6 @@ const About = ({ location }) => {
     <Layout location={location} title="About">
       <SEO title="Projects" />
       <header className="masthead" style={{ backgroundImage: `url(${aboutBackground})` }}>
-        <div className="overlay"></div>
         <div className="container">
           <div className="row">
             <div className="col-lg-8 col-md-10 mx-auto">
@@ -65,6 +66,11 @@ const About = ({ location }) => {
                     <div className="mt-2 text-muted">
                       <a href={project.link}>Website</a> - <a href={project.repo}>Source Code</a>
                     </div>
+                    {project.playStore && <div>
+                      <a href={`${project.playStore}&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1`}>
+                        <img style={{ maxWidth: "10rem" }} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+                      </a>
+                    </div>}
                 </div>
             ))}
           </div>
