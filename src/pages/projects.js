be5dfdfbc0b78e562dae095621d7b92829ef5993
adueck <adueck@users.noreply.github.com>
2024-03-06 14:26:42 -0500

    update

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,48 +1,51 @@
-import React from "react";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import aboutBackground from "../assets/images/about-bg.jpg";
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import aboutBackground from "../assets/images/about-bg.jpg"
 
 const projects = [
-    {
-        title: "Pashto Grammar",
-        description: "A Pashto grammar reference with interactive examples and quizzes.",
-        link: "https://grammar.lingdocs.com",
-        repo: "https://github.com/lingdocs/pashto-grammar",
-        playStore: "https://play.google.com/store/apps/details?id=com.lingdocs.pashto.grammar",
-    },
-    {
-        title: "Pashto Dictionary",
-        description: "A Pashto dicitionary with smart-search and inflection/conjugation",
-        link: "https://dictionary.lingdocs.com",
-        repo: "https://github.com/lingdocs/pashto-dictionary",
-        playStore: "https://play.google.com/store/apps/details?id=com.lingdocs.pashto.dictionary",
-    },
-    {
-      title: "Pashto Inflector",
-      description: "An open source TypeScript/React library for Pashto inflection, verb conjugation, phrase generation, text conversion, and more.",
-      link: "https://pashto-inflector.lingdocs.com",
-      repo: "https://github.com/lingdocs/pashto-inflector",
-    },
-    {
-        title: "Pashtoodle",
-        description: "A Pashto version of the well-loved word guessing game",
-        link: "https://pashtoodle.lingdocs.com",
-        repo: "https://github.com/lingdocs/pashto-wordle",
-    },
-    {
-        title: "RTL EPUB Maker",
-        description: "Easily create EPUB e-book files with proper RTL support. Because right-to-left languages deserve nice e-books too.",
-        link: "https://rtl-epub-maker.lingdocs.com",
-        repo: "https://github.com/lingdocs/rtl-epub-maker",
-    },
-];
+  {
+    title: "Pashto Grammar",
+    description:
+      "A Pashto grammar reference with interactive examples and quizzes.",
+    link: "https://grammar.lingdocs.com",
+    repo: "https://github.com/lingdocs/pashto-grammar",
+    playStore:
+      "https://play.google.com/store/apps/details?id=com.lingdocs.pashto.grammar",
+  },
+  {
+    title: "Pashto Dictionary",
+    description:
+      "A Pashto dicitionary with smart-search and inflection/conjugation",
+    link: "https://dictionary.lingdocs.com",
+    repo: "https://github.com/lingdocs/pashto-dictionary",
+    playStore:
+      "https://play.google.com/store/apps/details?id=com.lingdocs.pashto.dictionary",
+  },
+  {
+    title: "Pashto Inflector",
+    description:
+      "An open source TypeScript/React library for Pashto inflection, verb conjugation, phrase generation, text conversion, and more.",
+    link: "https://pashto-inflector.lingdocs.com",
+    repo: "https://github.com/lingdocs/pashto-inflector",
+  },
+  {
+    title: "RTL EPUB Maker",
+    description:
+      "Easily create EPUB e-book files with proper RTL support. Because right-to-left languages deserve nice e-books too.",
+    link: "https://rtl-epub-maker.lingdocs.com",
+    repo: "https://github.com/lingdocs/rtl-epub-maker",
+  },
+]
 
 const About = ({ location }) => {
   return (
     <Layout location={location} title="About">
       <SEO title="Projects" />
-      <header className="masthead" style={{ backgroundImage: `url(${aboutBackground})` }}>
+      <header
+        className="masthead"
+        style={{ backgroundImage: `url(${aboutBackground})` }}
+      >
         <div className="container">
           <div className="row">
             <div className="col-lg-8 col-md-10 mx-auto">
@@ -57,21 +60,30 @@ const About = ({ location }) => {
       <div className="container">
         <div className="row">
           <div className="col-lg-8 col-md-10 mx-auto">
-            {projects.map((project) => (
-                <div key={project.title}>
-                    <a href={project.link}>
-                        <h2 className="post-title">{project.title}</h2>
-                    </a>
-                    <div>{project.description}</div>
-                    <div className="mt-2 text-muted">
-                      <a href={project.link}>Website</a> - <a href={project.repo}>Source Code</a>
-                    </div>
-                    {project.playStore && <div>
-                      <a href={`${project.playStore}&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1`}>
-                        <img style={{ maxWidth: "10rem" }} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
-                      </a>
-                    </div>}
+            {projects.map(project => (
+              <div key={project.title}>
+                <a href={project.link}>
+                  <h2 className="post-title">{project.title}</h2>
+                </a>
+                <div>{project.description}</div>
+                <div className="mt-2 text-muted">
+                  <a href={project.link}>Website</a> -{" "}
+                  <a href={project.repo}>Source Code</a>
                 </div>
+                {project.playStore && (
+                  <div>
+                    <a
+                      href={`${project.playStore}&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1`}
+                    >
+                      <img
+                        style={{ maxWidth: "10rem" }}
+                        alt="Get it on Google Play"
+                        src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                      />
+                    </a>
+                  </div>
+                )}
+              </div>
             ))}
           </div>
         </div>
@@ -80,4 +92,4 @@ const About = ({ location }) => {
   )
 }
 
-export default About;
+export default About
